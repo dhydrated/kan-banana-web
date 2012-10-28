d868af6ed394594054621c42ab3bd73b7efb532c
dhydrated <dhydrated@gmail.com>
2012-10-28 23:05:43 +0800

    updated index view

--- assets/js/home.js
+++ assets/js/home.js
@@ -17,15 +17,6 @@ $(document).ready(function(){
   };
 
 
-  KanBanana.Models.Status = Backbone.Model.extend({
-	  //urlRoot: '/project/{projectId}/status'
-  });
-  
-  KanBanana.Collections.Statuses = Backbone.Collection.extend({
-      model: KanBanana.Models.Status,
-	  //url: '/project/{projectId}/status'
-  });
-
   KanBanana.Models.Project = Backbone.Model.extend({
 	  urlRoot: '/services/project'
   });
@@ -33,6 +24,136 @@ $(document).ready(function(){
   KanBanana.Collections.Projects = Backbone.Collection.extend({
       model: KanBanana.Models.Project,
 	  url: '/services/project'
+  });
+  
+  KanBanana.Views.ProjectRow = Backbone.View.extend({
+	  tagName: "tr",
+      template: template('project-row'),
+      render: function() {
+          this.$el.html(this.template(this));
+          return this;
+      },
+      name: function() { 
+    	  return this.model.get('name');    
+      },
+      description: function() { 
+    	  return this.model.get('description');    
+      },
+  });
+
+  KanBanana.Views.GenericModelRow = Backbone.View.extend({
+	  tagName: "tr",
+      template: template('generic-model-row'),
+      render: function() {
+          this.$el.html(this.template(this));
+          return this;
+      },
+      name: function() { 
+    	  return this.model.get('name');    
+      },
+  });
+  
+  KanBanana.Views.StatusList = Backbone.View.extend({
+      initialize: function(){
+    	  this.fetch();
+      },
+      template: template('statuses-list'),
+      events: {
+    	  "click .create-status" : "launchStatusForm",
+    	  "click .save-status" : "saveStatus",
+    	  "click #kb-statuses-table .btn-remove" : "removeStatus",
+    	  "click #kb-statuses-table .btn-edit" : "launchEditStatusForm"
+      },
+      render: function() {
+          this.$el.html(this.template(this));
+    	  that = this;
+    	  this.statuses.each(function(status){
+			  var statusRow = new KanBanana.Views.StatusRow({id: status.get('id'), model: status});
+			  that.$('#kb-statuses-table').append(statusRow.render().el);
+		  },this);
+          return this;
+      },
+      fetch: function() {
+    	  
+    	  KanBanana.Collections.Statuses = Backbone.Collection.extend({
+    		  url: '/services/project/'+this.options.projectId+'/status'
+    	  });
+    	  
+    	  this.statuses = new KanBanana.Collections.Statuses(context);
+    	  var that = this;
+    	  this.statuses.fetch({
+    		  success: function(){
+    			  that.render();
+    		  },
+    		  error: function(){
+    			  console.log('error');
+    		  }
+    	  });
+      },
+      launchStatusForm: function(){
+    	  var form =  $('#status-form-modal');
+    	  form.modal();
+    	  
+    	  form.find('#status-name').val('');
+    	  form.find('#status-name').focus();
+      },
+      launchEditStatusForm: function(events){
+    	  
+    	  this.launchStatusForm();
+    	  domId = events.currentTarget.id;
+    	  selectedId = domId.replace('edit-','');
+    	  this.statuses.each(function(status){
+    		  if(status && status.get('id') == selectedId){
+		    	  var form =  $('#status-form-modal');
+		    	  form.modal();
+				  form.find('#status-id').val(project.get('id'));
+				  form.find('#status-name').val(project.get('name'));
+    		  }
+    	  });
+    	  
+      },
+      saveStatus: function(){
+    	  that = this;
+    	  var form =  $('#status-form-modal');
+    	  var statusId = form.find('#status-id').val();
+    	  
+    	  //new
+    	  if(statusId === ''){
+    		  var newStatus = new KanBanana.Models.Status({name: $('#project-name').val()});
+        	  newStatus.save({},{success: function(model, response) {
+        		  that.statuses.add(model);
+        		  var statusRow = new KanBanana.Views.StatusRow({id: newStatus.get('id'), model: newStatus});
+        		  that.$('#kb-statuses-table').append(statusRow.render().el);
+    		  }});
+    	  }
+    	  //update
+    	  else{
+    		  this.statuses.each(function(status){
+        		  if(status && status.get('id') == statusId){
+    		    	  var form =  $('#status-form-modal');
+    		    	  form.modal();
+    		    	  status.set('name', form.find('#status-name').val());
+    		    	  status.save({success: function(model, response) {
+        			  }});
+        		  }
+        	  });
+    	  }
+    	  
+    	  $('#status-form-modal').modal('hide');
+    	  
+    	  this.render();
+      },
+      removeStatus: function(events){
+    	  domId = events.currentTarget.id;
+    	  selectedId = domId.replace('remove-','');
+    	  this.statuses.each(function(status){
+    		  if(status && status.get('id') == selectedId){
+    			  status.destroy({success: function(model, response) {
+    				  $('#'+selectedId).remove();  
+    			  }});
+    		  }
+    	  });
+      },
   });
   
   KanBanana.Views.ProjectList = Backbone.View.extend({
@@ -135,22 +256,6 @@ $(document).ready(function(){
     	  });
       },
   });
-  
-
-  KanBanana.Views.ProjectRow = Backbone.View.extend({
-	  tagName: "tr",
-      template: template('project-row'),
-      render: function() {
-          this.$el.html(this.template(this));
-          return this;
-      },
-      name: function() { 
-    	  return this.model.get('name');    
-      },
-      description: function() { 
-    	  return this.model.get('description');    
-      },
-  });
 
   KanBanana.Views.ProjectView = Backbone.View.extend({
 	  template: template('project-view'),
@@ -160,7 +265,7 @@ $(document).ready(function(){
       render: function() {
     	  this.$el.empty();
           this.$el.append(this.template(this));
-          var projectList = new KanBanana.Views.ProjectList({el: "#project-table"});
+          new KanBanana.Views.ProjectList({el: "#project-table"});
           return this;
       }
   });
@@ -173,6 +278,7 @@ $(document).ready(function(){
       render: function() {
     	  this.$el.empty();
           this.$el.append(this.template(this));
+          new KanBanana.Views.StatusList({el: "#status-table", projectId: this.options.projectId});
           return this;
       }
   });
@@ -209,7 +315,8 @@ $(document).ready(function(){
 	    projectSettings: function(projectId) {
 	    	console.log('projectSettings');
 	    	console.log('projectId: ' + projectId);
-	    	new KanBanana.Views.ProjectSettingsView({el: this.el});
+	    	context = {el: this.el, projectId: projectId};
+	    	new KanBanana.Views.ProjectSettingsView(context);
 	    }
 	  });
 
